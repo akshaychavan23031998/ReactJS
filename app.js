3897482f131950982604eea416f2684733d5c7cd
Akshay Ram Chavan <akshayrchavan07@gmail.com>
2023-08-09 21:10:27 +0530

    Episode 03

--- app.js
+++ app.js
@@ -1,93 +1,67 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-
+import ReactDOM, { render } from 'react-dom';
+/*
+React Element:- 
+1st way:- Create a element using the React.
+React.createElement => React Element (Creates an Object) => when we Render with root => its becomes an HTML.
+const heading = React.createElement("h3", {id:"h3"}, "Namaste into the H3 Tag");
+console.log(heading);    //its an object i.e element.
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
+React.createElement => Creates an Object => when we Render with root => its becomes an HTML.
+*/
 
 /*
-EX-01: soo this is to create a simple single div with h4 tag in it, by React.
-<div>
-    <h4>Hello world From React !! </h4>
-</div>
-
-const reactheading = React.createElement("h4", {id:"heading"}, "Hello world From React !!");
-console.log(reactheading);      
-Note:- this nothing but the object itself, it means its NOT an any h4 tag, 
-then how its getting converted to h4 tag??  the answer is while rendering
-Note:- ReactElemet(this is a Object) ==> HTML(Browser Understand)
-
-const reactroot = ReactDOM.createRoot(document.getElementById("reactroot"));
-reactroot.render(reactheading);
-Note:- means while rendering my render method takes the complate object and converts it into the h4 tag 
-with the all the attributes & text need to be printed into it as we given above in 3 arguments. 
+2nd way:- Create a element using the JSX.
+JSX => bebal transpiles the code to => React.createElement => Creates an Object => when we Render with root => its becomes an HTML.
+const jsxheading = <h3 id="h3"> Namaste into jsxheading </h3>
+root.render(jsxheading);
 */
 
-
-/*EX-02: so how i can create the nested html by using react as below
-
-<div id="parent">
-    <div id="child">
-        <h4> I am Nested HTML File </h4>
-    </div>
-</div> 
-Note:- ReactElemet(this is a Object) ==> HTML(Browser Understand)
-
-const parent = React.createElement("div", {id:"parent"}, 
-    React.createElement("div", {id:"child"}, 
-        React.createElement("h4", {}, "I am Nested HTML File")
-    )
-);
-console.log(parent);
-reactroot.render(parent);
-*/
-
-
-/*EX-03: so how i can create the nested html by using react as below,
-now we are going to create 2 childerens then as in 3rd argument i.e in child 
-we need to create/pass the array of childrans.
-
-<div id="parent">
-    <div id="child">
-        <h4> I am Nested HTML h4 tag </h4>
-        <h2> I am Nested HTML h2 tag </h2>
-    </div>
-</div> 
-
-const parent = React.createElement("div", {id:"parent"}, 
-    React.createElement("div", {id:"child"}, 
-        [React.createElement("h4", {}, "I am Nested HTML h4 tag "),
-         React.createElement("h2", {}, "I am Nested HTML h2 tag ")
-    ])
-);
-console.log(parent);
-reactroot.render(parent);
-Note:- ReactElemet(this is a Object) ==> HTML(Browser Understand)
-*/
-/*EX-04: so what if i wanted to make like below then,
-<div id="parent">
-    <div id="child">
-        <h4> I am Nested HTML h4 tag </h4>
-        <h2> I am Nested HTML h2 tag </h2>
-    </div>
-    <div id="child2">
-        <h4> I am Nested HTML h4 tag </h4>
-        <h2> I am Nested HTML h2 tag </h2>
-    </div>
-</div>
-
-means under parent there are 2 childerns means i need to pass this
-into the array.
-
-const parent = React.createElement("div", {id:"parent"},[
-    React.createElement("div", {id:"child"}, 
-        [React.createElement("h4", {}, "I am Nested HTML h4 tag "),
-         React.createElement("h2", {}, "I am Nested HTML h2 tag ")
-    ]),
-    React.createElement("div", {id:"child2"}, 
-        [React.createElement("h4", {}, "I am Nested HTML h4 tag "),
-         React.createElement("h2", {}, "I am Nested HTML h2 tag ")
-    ])
-]);
-*/
 /*
-Note:- so if we have observed is kii if my html is becemeing complicated then my react code
-becomes very complicated & this code is not readable soo to make it readable and 
-simple thenn we need to use JSX. */
+Note:-
+soo what happen is kii when i render this with my root
+whatever is there is in root (index.html) it will be 
+replaced by [root.render(heading);]
+if its not rendered then we will see the h3 tag from index.html
+and the msg is Not Rendered.
+*/
+
+/*
+React Components:-
+const root = ReactDOM.createRoot(document.getElementById("root"));
+const HeadingComponet = () => {
+    return <h3> Namaste From HeadingComponet </h3>
+}
+All so can be written as.
+const HeadingComponet1 = () => (
+    <h3> Namaste From HeadingComponet1 </h3>
+);
+we can alsoo write this as below by removing the retrn work soo that it becomes in single line.
+const HeadingComponet2 = () => <h3> Namaste From HeadingComponet2 </h3>;
+naw how to render this components ??
+
+root.render(heading);                    // Render of Element.
+root.render(<HeadingComponet />);           // Render of components.
+root.render(<HeadingComponet1 />);
+root.render(<HeadingComponet2 />);
+*/
+
+const Titel = () => {
+    <h1 className='head' tabIndex="5"> Namaste from Title </h1>
+}
+//component composition:- a component under another componet.
+const number = 10000;
+const HeadingComponet = () => {
+    <div id='container'>
+        <Titel />   
+        <Title></Title>
+        {Titel()}
+        {number}                 
+        <h3 className='heading'> Namaste From HeadingComponet </h3>
+    </div>
+}
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<HeadingComponet />);
+
+
